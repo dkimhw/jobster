a6278bb5df3ba36cfe40035b507a629314771930
David Kim <dkhw90@gmail.com>
2024-01-20 19:28:00 +0100

    Add sorting, filtering, and pagination on backend

--- models/User.js
+++ models/User.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-console.log(require('dotenv').config());
 
 const UserSchema = new mongoose.Schema({
   name: {
